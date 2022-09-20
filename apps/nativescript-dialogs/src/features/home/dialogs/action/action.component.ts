f6be2ce40e2d87f9adec7972fb54d72d5bc25a2a
Brian Clow <brianrclow@gmail.com>
2022-09-20 09:38:40 -0600

    Cleanup action dialogs

--- apps/nativescript-dialogs/src/features/home/dialogs/action/action.component.ts
+++ apps/nativescript-dialogs/src/features/home/dialogs/action/action.component.ts
@@ -1,30 +1,97 @@
 import { Component } from '@angular/core';
-import { Dialogs } from '@nativescript/core'
+import { Color, Dialogs, Frame, isIOS } from '@nativescript/core';
 
 @Component({
   selector: 'ns-angular-samples-action',
   templateUrl: './action.component.html',
-  styleUrls: ['./action.component.scss']
+  styleUrls: ['./action.component.scss'],
 })
 export class ActionComponent {
-
-  showAction() {
+  showNativeScriptAction() {
     const actionOptions = {
       title: 'Race selection',
       message: 'Choose your race',
       cancelButtonText: 'Cancel',
       actions: ['Human', 'Elf', 'Dwarf', 'Orc', 'Unicorn'],
-      cancelable: true // Android only
-    }
+      cancelable: true, // Android only
+    };
 
-    Dialogs.action(actionOptions).then(result => {
-      console.log('Dialog result: ', result)
+    Dialogs.action(actionOptions).then((result) => {
+      console.log('Dialog result: ', result);
       if (result === 'Options1') {
         // Do action 1
       } else if (result === 'Option2') {
         // Do action 2
       }
-    })
+    });
   }
 
+  showNativeAction() {
+    if (isIOS) {
+      const alertController =
+        UIAlertController.alertControllerWithTitleMessagePreferredStyle(
+          'Native iOS Action',
+          'you can change the actions and colors',
+          UIAlertControllerStyle.ActionSheet
+        );
+
+      // Here are some premade styling. The destructive by default is red on IOS. You can select default for them all or use existing.
+      const editAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Edit',
+        UIAlertActionStyle.Default,
+        (arg: UIAlertAction) => {
+          //code implementation here
+        }
+      );
+      const deleteAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Delete',
+        UIAlertActionStyle.Default,
+        (arg: UIAlertAction) => {
+          // changing UIAlertActionStyle.Destructive to UIAlertActionStyle.Default allowed for the tint to be updated down below
+          //code implementation here
+        }
+      );
+      const cancelAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Cancel',
+        UIAlertActionStyle.Cancel,
+        (arg: UIAlertAction) => {
+          //code implementation here
+        }
+      );
+
+      alertController.addAction(editAction);
+      alertController.addAction(deleteAction);
+      alertController.addAction(cancelAction);
+
+      // This is how you can force change the color of the title text on the actions (buttons).
+      alertController.view.tintColor = new Color('#000000').ios; // Color is a class in Nativescript, if we you want the Native IOS value, this is how you do it.
+
+      // alertController.editButtonItem.tintColor = new Color("#000000").ios;
+
+      // alertController = new Color("#000000").ios;
+
+      // editAction.setValueForKey(new Color('black').ios, 'titleTextColor');
+
+      const currentPage = Frame.topmost().currentPage;
+      const viewController: UIViewController = currentPage.ios;
+      viewController.presentModalViewControllerAnimated(alertController, true);
+    } else {
+      const actionOptions = {
+        title: 'Native Android Action',
+        message: 'not supported yet...',
+        cancelButtonText: 'Cancel',
+        actions: ['iOS simulator', 'physical device'],
+        cancelable: true, // Android only
+      };
+
+      Dialogs.action(actionOptions).then((result) => {
+        console.log('Dialog result: ', result);
+        if (result === 'Options1') {
+          // Do action 1
+        } else if (result === 'Option2') {
+          // Do action 2
+        }
+      });
+    }
+  }
 }
--- apps/nativescript-dialogs/src/features/home/dialogs/alert/alert.component.ts
+++ apps/nativescript-dialogs/src/features/home/dialogs/alert/alert.component.ts
@@ -5,39 +5,53 @@ import { Frame } from '@nativescript/core/ui/frame';
 @Component({
   selector: 'ns-angular-samples-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  styleUrls: ['./alert.component.scss'],
 })
 export class AlertComponent {
-
   showDefaultAlertDialog() {
     const alertOptions = {
       title: 'Warning',
       message: 'Something terrible has happened.',
       okButtonText: 'Okay',
-      cancelable: false // [Android only] Gets or sets if the dialog can be canceled by taping outside of the dialog.
-    }
+      cancelable: false, // [Android only] Gets or sets if the dialog can be canceled by taping outside of the dialog.
+    };
 
     Dialogs.alert(alertOptions).then(() => {
-      console.log('Race chosen!')
-    })
+      console.log('Race chosen!');
+    });
   }
 
   showIOSAlertDialog() {
-
-
     if (isIOS) {
-      const alertController = UIAlertController.alertControllerWithTitleMessagePreferredStyle("title", "message", UIAlertControllerStyle.Alert);
+      const alertController =
+        UIAlertController.alertControllerWithTitleMessagePreferredStyle(
+          'title',
+          'message',
+          UIAlertControllerStyle.Alert
+        );
 
       // Here are some premade styling. The destructive by default is red on IOS. You can select default for them all or use existing.
-      const editAction = UIAlertAction.actionWithTitleStyleHandler("Edit", UIAlertActionStyle.Default, (arg: UIAlertAction) => {
-        //code implementation here
-      });
-      const deleteAction = UIAlertAction.actionWithTitleStyleHandler("Delete", UIAlertActionStyle.Destructive, (arg: UIAlertAction) => {
-        //code implementation here
-      });
-      const cancelAction = UIAlertAction.actionWithTitleStyleHandler("Cancel", UIAlertActionStyle.Cancel, (arg: UIAlertAction) => {
-        //code implementation here
-      });
+      const editAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Edit',
+        UIAlertActionStyle.Default,
+        (arg: UIAlertAction) => {
+          //code implementation here
+        }
+      );
+      const deleteAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Delete',
+        UIAlertActionStyle.Destructive,
+        (arg: UIAlertAction) => {
+          //code implementation here
+        }
+      );
+      const cancelAction = UIAlertAction.actionWithTitleStyleHandler(
+        'Cancel',
+        UIAlertActionStyle.Cancel,
+        (arg: UIAlertAction) => {
+          //code implementation here
+        }
+      );
 
       alertController.addAction(editAction);
       alertController.addAction(deleteAction);
@@ -52,55 +66,11 @@ export class AlertComponent {
 
       // editAction.setValueForKey(new Color('red').ios, 'titleTextColor');
 
-
-
       const currentPage = Frame.topmost().currentPage;
       const viewController: UIViewController = currentPage.ios;
       viewController.presentModalViewControllerAnimated(alertController, true);
     }
   }
-
-
-  showIOSActionSheetDialog() {
-
-
-    if (isIOS) {
-      const alertController = UIAlertController.alertControllerWithTitleMessagePreferredStyle("title", "message", UIAlertControllerStyle.ActionSheet);
-
-      // Here are some premade styling. The destructive by default is red on IOS. You can select default for them all or use existing.
-      const editAction = UIAlertAction.actionWithTitleStyleHandler("Edit", UIAlertActionStyle.Default, (arg: UIAlertAction) => {
-        //code implementation here
-      });
-      const deleteAction = UIAlertAction.actionWithTitleStyleHandler("Delete", UIAlertActionStyle.Default, (arg: UIAlertAction) => { // changing UIAlertActionStyle.Destructive to UIAlertActionStyle.Default allowed for the tint to be updated down below
-        //code implementation here
-      });
-      const cancelAction = UIAlertAction.actionWithTitleStyleHandler("Cancel", UIAlertActionStyle.Cancel, (arg: UIAlertAction) => {
-        //code implementation here
-      });
-
-      alertController.addAction(editAction);
-      alertController.addAction(deleteAction);
-      alertController.addAction(cancelAction);
-
-      // This is how you can force change the color of the title text on the actions (buttons).
-      alertController.view.tintColor = new Color("#000000").ios; // Color is a class in Nativescript, if we you want the Native IOS value, this is how you do it.
-
-      // alertController.editButtonItem.tintColor = new Color("#000000").ios;
-
-      // alertController = new Color("#000000").ios;
-
-      // editAction.setValueForKey(new Color('black').ios, 'titleTextColor');
-
-
-
-      const currentPage = Frame.topmost().currentPage;
-      const viewController: UIViewController = currentPage.ios;
-      viewController.presentModalViewControllerAnimated(alertController, true);
-    }
-  }
-
-
-
 
 
 }
